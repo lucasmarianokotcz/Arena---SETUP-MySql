b5ebc7bb3ec2ab19e2cea881b5f92c3275c2d646
Lucas Mariano Kotcz <lucasmariano-@hotmail.com>
2020-06-22 02:16:23 -0300

    Adicionado projeto de testes e método de teste do MonstroBLL, método Select, teste bem-sucedido.

---
+++ Testes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testes")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7943927e-31d2-439f-8828-4c1d654c1bf5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
