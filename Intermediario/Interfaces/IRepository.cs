a66b8d2f8bda7524c7186b2314e3f7f87f5bb76c
Lucas Mariano Kotcz <lucasmariano-@hotmail.com>
2020-07-06 01:12:41 -0300

    Estrutura inicial para rework definida (vide TODO.text).

--- Intermediario/Interfaces/IRepository.cs
+++ Intermediario/Interfaces/IRepository.cs
@@ -4,9 +4,6 @@ namespace Intermediario.Interfaces
 {
     internal interface IRepository<T> where T : class
     {
-        bool Insert(T obj);
-        bool Update(T obj);
-        bool Delete(int id);
         List<T> Select();
     }
 }
